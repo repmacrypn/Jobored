dbc717059ac0d8c62fb56818f558078d9d073cb6
repmacrypn <artik3267@gmail.com>
2023-05-14 00:06:46 +0300

    core project data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
+import HeaderContainer from './components/header/HeaderContainer';
+import VacanciesContainer from './components/vacancies/VacanciesContainer';
+import FavouritesContainer from './components/favourites/FavouritesContainer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <HeaderContainer />
+      <div className='main'>
+        <Routes>
+          <Route path='/' element={<Navigate to='/vacancies' />} />
+          <Route path='/vacancies/*' element={<VacanciesContainer />} />
+          <Route path='/favourites/*' element={<FavouritesContainer />} />
+          <Route path='*' element={<div className='demoMessageAlert'>404 NOT FOUND</div>} />
+        </Routes>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,13 +3,27 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import store, { persistor } from './redux/store';
+import { Provider } from 'react-redux';
+import { MantineProvider } from '@mantine/core';
+import { PersistGate } from 'redux-persist/integration/react'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <MantineProvider withGlobalStyles withNormalizeCSS>
+    <BrowserRouter>
+      <Provider store={store}>
+        <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
+          <App store={store} />
+        </PersistGate>
+      </Provider>
+    </BrowserRouter>
+  </MantineProvider>
 );
+
+window.store = store;
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
