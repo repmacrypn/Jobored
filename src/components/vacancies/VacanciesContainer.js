df127dcd93a5e400164ab9dfa31c62b33a06b42c
repmacrypn <artik3267@gmail.com>
2023-05-17 01:55:11 +0300

    different shlak

--- src/components/vacancies/VacanciesContainer.js
+++ src/components/vacancies/VacanciesContainer.js
@@ -7,6 +7,7 @@ import {
 import { modifyFavArray, setFavTotalCount } from '../../redux/favReducer';
 import starFavImage from '../../resources/images/StarFav.png';
 import starNotFavImage from '../../resources/images/StarNotFav.png';
+import starOnHoverImage from '../../resources/images/StarOnHover.png';
 import Preloader from '../common components/preloader/Preloader';
 import Paginator from '../../utilites/Paginator';
 import FilterForm from "./forms/FilterForm";
@@ -16,6 +17,7 @@ import '../../styles/defaultStyles.css';
 const VacanciesContainer = (props) => {
     const [paginatorPortionNum, setPaginatorPortionNum] = useState(Math.ceil(props.currentPage === 0 ?
         1 : props.currentPage / props.portionSise));
+    const [isFocused, setIsFocused] = useState(false);
 
     useEffect(() => {
         props.getVacanciesOnFieldLoad(props.count, props.currentPage, props.catalogue,
@@ -38,8 +40,23 @@ const VacanciesContainer = (props) => {
             props.setFavTotalCount(++props.favourites.length)
     };
 
+    const processSalaryFieldAccom = (from, to) => {
+        let result = 'з/п ';
+        if (from && to) {
+            result += `${from} - ${to} `;
+        } else if (from) {
+            result += `от ${from} `;
+        } else if (to) {
+            result += `${to} `;
+        } else {
+            result = 'не указана';
+        }
+
+        return result;
+    };
+
     return (
-        <div className={`_mainContentField ${s.vacanciesMainField}`}>
+        <div className={`_mainContentField ${s.vacanciesWrapperField}`}>
             <div className={s.filterField}>
                 <FilterForm
                     setPaginatorPortionNum={setPaginatorPortionNum}
@@ -57,46 +74,72 @@ const VacanciesContainer = (props) => {
                 {
                     props.isFetching ?
                         <Preloader /> :
-                        <div>
-                            <Paginator
-                                onPageChange={onPageChange}
-                                pageSize={props.count}
-                                totalItemsCount={props.totalCount}
-                                currentPage={props.currentPage === 0 ? 1 : props.currentPage}
-                                paginatorPortionNum={paginatorPortionNum}
-                                setPaginatorPortionNum={setPaginatorPortionNum}
-                            />
-                            {
-                                props.vacancies.map(obj => {
-                                    return <div key={obj.id}>
-                                        {obj.profession} - {obj.firm_name} - {obj.town?.title} - {obj.catalogues[0]?.title} - {obj.type_of_work?.title}
-                                        - {obj.payment_to}:{obj.payment_from}/{obj.currency}
-                                        {
-                                            props.favourites.find(fav => {
-                                                return fav.id === obj.id;
-                                            }) ?
-                                                <span onClick={() => onFavButtonClick(obj, true)}>
-                                                    <img
-                                                        height='22px'
-                                                        width='22px'
-                                                        alt='remove from favourites'
-                                                        src={starFavImage}
-                                                    />
-                                                </span>
-                                                :
-                                                <span onClick={() => onFavButtonClick(obj, false)}>
-                                                    <img
-                                                        height='22px'
-                                                        width='22px'
-                                                        alt='add to favourites'
-                                                        src={starNotFavImage}
-                                                    />
-                                                </span>
-                                        }
-                                    </div>
-                                })
-                            }
-                        </div>
+                        <>
+                            <div>
+                                {
+                                    props.vacancies.map(obj => {
+                                        return <div className={s.vacancy} key={obj.id}>
+                                            <div className={`${s.profession} titleSSemiBold`}>
+                                                {obj.profession}
+                                            </div>
+                                            <div className={s.workConditField}>
+                                                <div className={s.salary}>
+                                                    {processSalaryFieldAccom(obj.payment_from, obj.payment_to)}
+                                                    {obj.currency}
+                                                </div>
+                                                <div className={s.typeOfWork}>
+                                                    {obj.type_of_work?.title}
+                                                </div>
+                                            </div>
+                                            <div className={s.town}>
+                                                {obj.town?.title || 'Не указан'}
+                                            </div>
+                                            {
+                                                props.favourites.find(fav => {
+                                                    return fav.id === obj.id;
+                                                }) ?
+                                                    <div className={s.favStar} onClick={() => onFavButtonClick(obj, true)}>
+                                                        <img
+                                                            height='22px'
+                                                            width='22px'
+                                                            alt='remove from favourites'
+                                                            src={starFavImage}
+                                                        />
+                                                    </div>
+                                                    :
+                                                    <div onBlur={() => setIsFocused(false)} onFocus={() => setIsFocused(true)} className={`${s.favStar}`} onClick={() => onFavButtonClick(obj, false)}>
+                                                        {
+                                                            isFocused ?
+                                                                <img
+                                                                    height='22px'
+                                                                    width='22px'
+                                                                    alt='star on hover'
+                                                                    src={starOnHoverImage}
+                                                                /> :
+                                                                <img
+                                                                    height='22px'
+                                                                    width='22px'
+                                                                    alt='add to favourites'
+                                                                    src={starNotFavImage}
+                                                                />
+                                                        }
+                                                    </div>
+                                            }
+                                        </div>
+                                    })
+                                }
+                            </div>
+                            <div className={s.paginator}>
+                                <Paginator
+                                    onPageChange={onPageChange}
+                                    pageSize={props.count}
+                                    totalItemsCount={props.totalCount}
+                                    currentPage={props.currentPage === 0 ? 1 : props.currentPage}
+                                    paginatorPortionNum={paginatorPortionNum}
+                                    setPaginatorPortionNum={setPaginatorPortionNum}
+                                />
+                            </div>
+                        </>
                 }
             </div>
         </div>
