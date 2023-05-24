d8e3c0b6f31242cf3716b73d9e3a73a94edc0817
repmacrypn <artik3267@gmail.com>
2023-05-24 21:52:45 +0300

    upd index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter, HashRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import store, { persistor } from './redux/store';
 import { Provider } from 'react-redux';
 import { MantineProvider } from '@mantine/core';
