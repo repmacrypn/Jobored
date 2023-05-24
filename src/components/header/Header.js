84310eeea2d4ba14fa5ae3d160852917b86ae6e4
repmacrypn <artik3267@gmail.com>
2023-05-24 23:30:10 +0300

    header css changes

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,22 +3,24 @@ import { NavLink } from "react-router-dom";
 import s from './Header.module.css';
 
 const Header = () => {
-    return <header className={s.header}>
-        <div className={s.logoTitle}>
-            Jobored
+    return <header className={s.headerWrapper}>
+        <div className={s.header}>
+            <div className={s.logoTitle}>
+                Jobored
+            </div>
+            <nav className={s.nav}>
+                <HeaderNavItem
+                    className='vacancies'
+                    text='Поиск Вакансий'
+                    to='vacancies'
+                />
+                <HeaderNavItem
+                    className='fav'
+                    text='Избранное'
+                    to='favourites'
+                />
+            </nav>
         </div>
-        <nav className={s.nav}>
-            <HeaderNavItem
-                className='vacancies'
-                text='Поиск Вакансий'
-                to='vacancies'
-            />
-            <HeaderNavItem
-                className='fav'
-                text='Избранное'
-                to='favourites'
-            />
-        </nav>
     </header>;
 };
 
