dfe67393265ba22d4a4415f2a5ef5c8dcb52ee6f
repmacrypn <artik3267@gmail.com>
2023-08-01 11:56:51 +0300

    ts injected

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Provider } from 'react-redux'
 import { MantineProvider } from '@mantine/core'
 import { PersistGate } from 'redux-persist/integration/react'
@@ -9,19 +8,19 @@ import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { store, persistor } from './redux/store'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
-  <MantineProvider withGlobalStyles withNormalizeCSS>
+  <MantineProvider withGlobalStyles withNormalizeCSS >
     <HashRouter>
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
+      <Provider store={store} >
+        <PersistGate loading={null} persistor={persistor} >
           <App />
         </PersistGate>
       </Provider>
     </HashRouter>
   </MantineProvider>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
