d0380d983c0c9e1df84930ecd7839fdd73505d23
repmacrypn <artik3267@gmail.com>
2023-11-12 20:31:44 +0300

    feat: getting started with prettier/eslint fix: aliases changed

--- src/components/favourites/Favourites.tsx
+++ src/components/favourites/Favourites.tsx
@@ -1,59 +1,59 @@
 import { useEffect } from 'react'
+
+import { EmptyState } from '@/components/common components/emptyState/EmptyState'
+import { FavPagination } from '@/components/common components/paginator/Paginator'
+import { FavStar, VacancyData } from '@/components/vacancies/Vacancies'
+import { useAppDispatch, useAppSelector } from '@/hooks/useAppHooks'
+import { getFavourites, selectFav } from '@/redux/favSlice'
+import { IVacancy } from '@/types/vacancy.interface'
+
 import s from './Favourites.module.css'
-import { FavStar, VacancyData } from '@components/vacancies/Vacancies'
-import { EmptyState } from '@components/common components/emptyState/EmptyState'
-import { getFavourites, selectFav } from '../../redux/favSlice'
-import { useAppDispatch, useAppSelector } from '../../hooks/useAppHooks'
-import { FavPagination } from '@components/common components/paginator/Paginator'
-import { IVacancy } from '../../types/vacancy.interface'
 
 export const Favourites = () => {
-    const dispatch = useAppDispatch()
-    const favourites = useAppSelector(selectFav)
-    const page = useAppSelector(state => state.favourites.page)
+  const dispatch = useAppDispatch()
+  const favourites = useAppSelector(selectFav)
+  const page = useAppSelector((state) => state.favourites.page)
 
-    const itemsPerPage = 4
+  const itemsPerPage = 4
 
-    useEffect(() => {
-        dispatch(getFavourites(JSON.parse(JSON.stringify(favourites))
-            .slice(page * itemsPerPage, page * itemsPerPage + itemsPerPage)))
-    }, [page, dispatch, favourites])
-
-    if (!favourites.length) {
-        return <EmptyState isFS={true} />
-    }
-
-    return (
-        <div className={s.contentFiled}>
-            <CurrentFavArray />
-            <FavPagination
-                totalCount={favourites.length}
-                itemsPerPage={itemsPerPage}
-                pageNumber={page}
-            />
-        </div>
+  useEffect(() => {
+    dispatch(
+      getFavourites(
+        JSON.parse(JSON.stringify(favourites)).slice(
+          page * itemsPerPage,
+          page * itemsPerPage + itemsPerPage,
+        ),
+      ),
     )
+  }, [page, dispatch, favourites])
+
+  if (!favourites.length) {
+    return <EmptyState isFS />
+  }
+
+  return (
+    <div className={s.contentFiled}>
+      <CurrentFavArray />
+      <FavPagination
+        totalCount={favourites.length}
+        itemsPerPage={itemsPerPage}
+        pageNumber={page}
+      />
+    </div>
+  )
 }
 
 const CurrentFavArray = () => {
-    const currentFavArray = useAppSelector(state => state.favourites.currentFavArray)
+  const currentFavArray = useAppSelector((state) => state.favourites.currentFavArray)
 
-    const content = currentFavArray.map((obj: IVacancy) => {
-        return (
-            <div
-                className={s.vacancy}
-                key={obj.id}
-            >
-                <VacancyData
-                    isDefault={true}
-                    obj={obj}
-                />
-                <FavStar
-                    obj={obj}
-                />
-            </div>
-        )
-    })
+  const content = currentFavArray.map((obj: IVacancy) => {
+    return (
+      <div className={s.vacancy} key={obj.id}>
+        <VacancyData isDefault obj={obj} />
+        <FavStar obj={obj} />
+      </div>
+    )
+  })
 
-    return <div>{content}</div>
+  return <div>{content}</div>
 }
