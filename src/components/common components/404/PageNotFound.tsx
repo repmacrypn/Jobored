8454449fe2f552588fea4a663fd405a42840b394
repmacrypn <artik3267@gmail.com>
2023-11-16 22:50:13 +0300

    fix: components refactoring

--- src/components/common components/404/PageNotFound.tsx
+++
@@ -1,8 +0,0 @@
-import '@/styles/defaultStyles.css'
-import s from './PageNotFound.module.css'
-
-const PageNotFound = () => {
-  return <div className={`titleSSemiBold ${s.demoMessageAlert}`}>404 NOT FOUND xD</div>
-}
-
-export default PageNotFound
--- src/pages/PageNotFound/index.tsx
+++
@@ -1,6 +0,0 @@
-import '@/styles/defaultStyles.css'
-import s from './styles.module.scss'
-
-export const PageNotFound = () => {
-  return <div className={`titleSSemiBold ${s.demoMessageAlert}`}>404 NOT FOUND xD</div>
-}
